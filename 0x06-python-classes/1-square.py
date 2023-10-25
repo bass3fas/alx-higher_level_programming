ed5253f6aa29f0c3025ebf55f5190dac9a5a54dd
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-10-24 23:56:43 -0700

    self

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Define a class square"""
+class Square:
+    """ represents a square"""
+    def __init__(self, size):
+        """ Intialize a square
+        Args:
+        size(int): size of the new square
+        """
+        self.__size = size
+        
