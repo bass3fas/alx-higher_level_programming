7f6c225ba836d838be49bd956ca08cec7af6d770
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-11-08 00:56:56 -0800

    task7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Validates integer"""
+        if not type(value) == int:
+            raise TypeError("{} must be an integer".format(name)
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
