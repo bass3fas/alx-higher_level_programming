4d338c1e187666698ef35d7f7f682657041b2fb3
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-11-13 02:12:44 -0800

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """"Defines a Class"""
-
+import json
 
 class Base:
     """to manage id attribute in all your future classes 
@@ -12,3 +12,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return '[]'
+        return json.dumps(list_dictionaries)
+    
