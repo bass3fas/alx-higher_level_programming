868ea7dad8f112a9ddacdf4b80cdb5c55e5a5949
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-11-12 23:45:14 -0800

    rectangle update method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,6 +69,23 @@ class Rectangle(Base):
         for j in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
+
+
+    """updating the argument to each attribute"""
+    def update(self, *args):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+
+                
     """updating string method"""
     def __str__(self):
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
