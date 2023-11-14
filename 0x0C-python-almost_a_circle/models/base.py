4c8e1cdd9a5191ae8c3f1f2c2597edb31d4cbd48
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-11-13 20:58:31 -0800

    fix identation

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,9 +16,9 @@ class Base:
         Args:
             id (int): The identity of the new Base.
         """
-        if id is not None:
-            self.id = id
-        else:
+         if id is not None:
+             self.id = id
+         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
