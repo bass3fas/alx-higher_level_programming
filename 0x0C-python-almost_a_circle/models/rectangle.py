cd28b1de98d7d33a60a7be23484d2f544a15b3f1
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-11-13 20:47:16 -0800

    documenting rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,20 @@ from models.base import Base
 class Rectangle(Base):
     """defines a rectangle"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
         super().__init__(id)
         self.__width = width
         self.__height = height
@@ -14,10 +28,12 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """width setter"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value <= 0:
@@ -26,10 +42,12 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """height getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """height setter"""
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value <= 0:
@@ -38,10 +56,12 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """x coordinate getter"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """x coordinate setter"""
         if type(value) != int:
             raise TypeError("x must be an integer")
         elif value < 0:
@@ -50,10 +70,12 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """y coordinate getter"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """y coordinate setter"""
         if type(value) != int:
             raise TypeError("y must be an integer")
         elif value < 0:
@@ -61,15 +83,19 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """area getter"""
         return self.__width * self.__height
 
     def display(self):
+        """displays rectangle withtin 
+        correct coordinates by hash"""
         for i in range(self.__y):
             print()
         for j in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
     def update(self, *args, **kwargs):
+        """updating arguments"""
         if args:
             if len(args) >= 1:
                 self.id = args[0]
@@ -86,6 +112,7 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
     def to_dictionary(self):
+        """"dictionary creator"""
         return {
             'id': self.id,
             'width': self.__width,
@@ -95,6 +122,7 @@ class Rectangle(Base):
         }
 
     def __str__(self):
+        """instance representation"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
