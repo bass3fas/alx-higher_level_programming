1ce135669ae0b0ae05d00757d4115f02ff1cbcd2
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2024-03-03 20:38:05 -0800

    doc

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+ """takes in a URL and an email address,
+ - sends a POST request to the passed URL with the email as a parameter
+ -displays the body of the response."""
 import requests
 import sys
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
